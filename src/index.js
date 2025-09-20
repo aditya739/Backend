019966c3711ff5c2b9bc44087090a5962fb15a49
aditya739 <adityasingh11121@gmail.com>
2025-09-20 23:58:44 +0530

    connect mongo atlas

---
+++ src/index.js
@@ -0,0 +1,47 @@
+// for using import in dotenv  we use config
+//otherwise it will not work,if  dont wont to use this we can  usre require
+// const dotenv = require("dotenv");
+// dotenv.config({path: "./.env"});
+
+import dotenv from "dotenv";
+dotenv.config({path: "./.env"});
+
+import mongoose from "mongoose";
+import{ DB_NAME} from "./constants.js";
+
+import connectDb from "./db/index.js";
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//USING IFI FOR ASYNC AWAIT
+(async () => {
+    try{
+
+        await mongoose.connect('${process.env.MONGODB_URL}/${DB_NAME}');
+        app.on("error",(error)=>{
+            console.log("ERROR:",error);
+        })
+}
+
+app.listen(process.env.PORT,()=>{
+    console.log("Server is running on port",process.env.PORT);
+})
+catch(error){
+    console.error("ERROR:", error);
+    throw error;
+}
+}
+)()
+*/
