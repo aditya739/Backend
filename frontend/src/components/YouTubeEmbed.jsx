272d2a96be0f9d2eba2354acbc5125c622789af9
aditya739 <adityasingh11121@gmail.com>
2025-09-29 09:35:50 +0530

    Fix YouTube embed blocking and SPA routing issues

--- frontend/src/components/YouTubeEmbed.jsx
+++ frontend/src/components/YouTubeEmbed.jsx
@@ -6,7 +6,7 @@ export default function YouTubeEmbed({ videoId, title = "YouTube video" }) {
     <div className="aspect-video w-full overflow-hidden rounded bg-black">
       <iframe
         className="w-full h-full"
-        src={`https://www.youtube.com/embed/${videoId}`}
+        src={`https://www.youtube-nocookie.com/embed/${videoId}?rel=0&modestbranding=1&iv_load_policy=3`}
         title={title}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
